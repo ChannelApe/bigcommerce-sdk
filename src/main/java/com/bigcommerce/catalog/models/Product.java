428311f32fb9ac98486158efe89f6ac42e3aee4d
Ronald Brachetti <ronald_brachetti@mckinsey.com>
2018-05-09 14:22:26 +0200

    Ignore unknown properties for product entities

--- src/main/java/com/bigcommerce/catalog/models/Product.java
+++ src/main/java/com/bigcommerce/catalog/models/Product.java
@@ -9,12 +9,14 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 @JsonInclude(Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Product {
 
 	private Integer id;
