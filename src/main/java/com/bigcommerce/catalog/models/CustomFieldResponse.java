6794dfdb36c7dd3ae4c6315e0bc20a9164c10e17
Rashiq Zahid <rashiq.z@gmail.com>
2018-05-16 19:18:31 +0200

    Ignore meta

--- src/main/java/com/bigcommerce/catalog/models/CustomFieldResponse.java
+++ src/main/java/com/bigcommerce/catalog/models/CustomFieldResponse.java
@@ -1,6 +1,7 @@
 package com.bigcommerce.catalog.models;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,11 +9,22 @@ import java.util.List;
 public class CustomFieldResponse {
 	private CustomField data;
 
+	@XmlTransient
+	private Meta meta = new Meta();
+
 	public CustomField getData() {
 		return data;
 	}
 
 	public void setData(CustomField data) {
 		this.data = data;
+	}
+
+	public Meta getMeta() {
+		return meta;
+	}
+
+	public void setMeta(Meta meta) {
+		this.meta = meta;
 	}
 }
