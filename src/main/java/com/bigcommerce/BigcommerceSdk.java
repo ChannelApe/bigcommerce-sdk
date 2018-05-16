0390937d0ebc46f452299c27c5a186dfb70978eb
Rashiq Zahid <rashiq.z@gmail.com>
2018-05-16 15:42:20 +0200

    Change type of customFieldId to String

--- src/main/java/com/bigcommerce/BigcommerceSdk.java
+++ src/main/java/com/bigcommerce/BigcommerceSdk.java
@@ -247,13 +247,13 @@ public class BigcommerceSdk {
 		delete(webTarget, Object.class);
 	}
 
-	public void deleteProductCustomField(final Integer productId, final Integer customFieldId) {
+	public void deleteProductCustomField(final Integer productId, final String customFieldId) {
 		final WebTarget webTarget = baseWebTargetV3
 			.path(CATALOG)
 			.path(PRODUCTS)
 			.path(String.valueOf(productId))
 			.path(CUSTOM_FIELDS_PATH)
-			.path(String.valueOf(customFieldId));
+			.path(customFieldId);
 		delete(webTarget, Object.class);
 	}
 
